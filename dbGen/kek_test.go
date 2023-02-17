f19a87456a78f0dbc3c7e0bcdf7405f76261955c
jlortiz0 <jlortiz0@users.noreply.github.com>
2023-02-16 23:50:39 -0800

    Benchmark kek insert instead

--- dbGen/kek_test.go
+++ dbGen/kek_test.go
@@ -3,6 +3,7 @@ package main_test
 import "testing"
 
 func BenchmarkKekGuilds(b *testing.B) {
+	b.SkipNow()
 	db := setupHelper(b)
 	defer db.Close()
 	stmt, err := db.Prepare("SELECT * FROM kekGuilds WHERE gid=?001;")
@@ -15,20 +16,40 @@ func BenchmarkKekGuilds(b *testing.B) {
 	})
 }
 
-func BenchmarkKekGuilds2(b *testing.B) {
+func BenchmarkKekInsert2(b *testing.B) {
 	db := setupHelper(b)
 	defer db.Close()
-	stmt, err := db.Prepare("SELECT * FROM kekGuilds2 WHERE gid=?001;")
+	db.Exec("INSERT OR REPLACE INTO kekUsers (uid) VALUES (?);", 300)
+	db.Exec("DELETE FROM kekMsgs WHERE uid = ?;", 300)
+	stmt, err := db.Prepare("INSERT INTO kekMsgs2 (uid, mid, score) VALUES (?001, ?002, ?003) ON CONFLICT DO UPDATE SET score=excluded.score;")
 	checkFatal(err)
 	defer stmt.Close()
 	b.RunParallel(func(p *testing.PB) {
 		for p.Next() {
-			stmt.Exec(1234)
+			_, err := stmt.Exec(300, 1, 2)
+			checkFatal(err)
+		}
+	})
+}
+
+func BenchmarkKekInsert(b *testing.B) {
+	db := setupHelper(b)
+	defer db.Close()
+	db.Exec("INSERT OR REPLACE INTO kekUsers (uid) VALUES (?);", 300)
+	db.Exec("DELETE FROM kekMsgs WHERE uid = ?;", 300)
+	stmt, err := db.Prepare("INSERT INTO kekMsgs (uid, mid, score) VALUES (?001, ?002, ?003) ON CONFLICT DO UPDATE SET score=excluded.score;")
+	checkFatal(err)
+	defer stmt.Close()
+	b.RunParallel(func(p *testing.PB) {
+		for p.Next() {
+			_, err := stmt.Exec(300, 1, 2)
+			checkFatal(err)
 		}
 	})
 }
 
 func BenchmarkKekCombine(b *testing.B) {
+	b.SkipNow()
 	db := setupHelper(b)
 	defer db.Close()
 	stmt, err := db.Prepare(`UPDATE kekUsers SET score = score + m.total FROM (
@@ -43,13 +64,15 @@ func BenchmarkKekCombine(b *testing.B) {
 		for p.Next() {
 			tx, err := db.Begin()
 			checkFatal(err)
-			tx.Stmt(stmt).Exec(348976)
+			_, err = tx.Stmt(stmt).Exec(348976)
+			checkFatal(err)
 			tx.Rollback()
 		}
 	})
 }
 
 func BenchmarkKekCombine2(b *testing.B) {
+	b.SkipNow()
 	db := setupHelper(b)
 	defer db.Close()
 	stmt, err := db.Prepare(`UPDATE kekUsers SET score = score + m.total FROM (
@@ -64,7 +87,8 @@ func BenchmarkKekCombine2(b *testing.B) {
 		for p.Next() {
 			tx, err := db.Begin()
 			checkFatal(err)
-			tx.Stmt(stmt).Exec(348976)
+			_, err = tx.Stmt(stmt).Exec(348976)
+			checkFatal(err)
 			tx.Rollback()
 		}
 	})
